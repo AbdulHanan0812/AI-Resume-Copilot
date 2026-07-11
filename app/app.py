9ccc474600d473f13e1d5841efb631fc014f5e80
AbdulHanan0812 <abdulhanan908583@gmail.com>
2026-07-11 16:43:31 +0500

    fix: added app path to sys.path for vercel imports

--- app/app.py
+++ app/app.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from flask import Flask, redirect, render_template, session, request, url_for, jsonify
 from db import Base, sessionLocal, engine
 from models import User, Report
